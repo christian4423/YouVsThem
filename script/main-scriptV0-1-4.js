fab0089ff5da9edd8548b4a32cd94a1cfb6e8fe9
christian4423 <cmatsoukis@gmail.com>
2015-05-04 13:12:01 -0400

    Bullet collision finished
    The main changes in this update was to make the collision detection way
    more accurate and to save memory.
    
    Collision detection will now:
    -check each bullet position.
    -if bullet hits nothing for two seconds it is deleted.
    -if bullet leaves the game world, it is deleted.
    
    Content:
    - Score is being redone and has been taken out as of now but will be
    added back in the next update
    - Rain has been removed, for now. It will be back next update.
    - Minor CSS changes.
    - Minor HTML changes.
    - <title> changed from "U-VS-THEM" => "YOU-VS-THEM"
    - Took code out of touch controls and had it go to the approiate
    function instead of rewriting code.
    - Renamed fire() function to bulletSpawn().

--- script/main-scriptV0-1-4.js
+++ script/main-scriptV0-1-4.js
@@ -5,6 +5,35 @@ function copyRight() {
     document.getElementById('copyRight-span').innerText = year;
 }
 
+var zombieSpawnSpeed = 1000;
+//touch
+$(function () {
+    $("#bg").bind("tap", bulletTapHandler);
+
+    function bulletTapHandler(event) {
+        bulletSpawn();
+    };
+
+    $("#right").bind("tap", rightTouchHandler);
+    function rightTouchHandler(event) {
+        rightArrowPressed()
+    }
+
+    $("#left").bind("tap", leftTouchHandler);
+
+    function leftTouchHandler(event) {
+        leftArrowPressed();
+    };
+    $(".startButton").bind("tap", startGameHandler);
+
+    function startGameHandler(event) {
+        $(this).css("visibility", "hidden")
+        $('.title').css("visibility", "hidden")
+        setInterval(function () { zombieSpawning() }, zombieSpawnSpeed);
+    };
+});
+
+
 //keyboard
 function leftArrowPressed() {
     var p = $("#player");
@@ -33,19 +62,7 @@ function rightArrowPressed() {
     } else { return false }
 
 }
-function bulletSpawn() {
-    $('#player').append('<div id="bullet" class="bullet">');
-    if (localStorage.getItem('facing') == "right") {
-        $('.bullet').animate({ "left": "1000px" }, 5000, "linear", function () {
-            $(this).remove();
-        });
-    } else if ((localStorage.getItem('facing') == "left")) {
-        $('.bullet').animate({ "right": "1000px" }, 5000, "linear", function () {
-            $(this).remove();
-        });
-    };
-    bulletHit()
-};
+
 function moveSelection(evt) {
     switch (evt.keyCode) {
         case 37:
@@ -72,61 +89,30 @@ function docReady() {
 
 var currentScore = 0;
 var i = 0
-//zombie birth and movement left and bottom movement
-
-
-    function zombieSpawning(){
-    var min = 3;
-        var max = 2000;
-        var s = Math.floor(Math.random() * (max - min + 1)) + min;
-        var p = $("#player");
-        var poff = p.offset();
-        var pleft = poff.left;
-        var difference = s - pleft;
-        var Maximus = pleft + 400;
-        var Minimus = pleft - 300
-
-        if (s >= Maximus) {
-            $('#zom').prepend('<div id="zombie" value="100"class="zombie">');
-            $('#zombie').css("left", s + "px")
-        } else if (s <= Minimus) {
-            $('#zom').prepend('<div id="zombie" value="100" class="zombie">');
-            $('#zombie').css("left", s + "px")
-        }
-        else {
-            return false;
-        }
-        var min = 5;
-        var max = 30;
-        $('#zombie').css("bottom", Math.floor(Math.random() * (max - min + 1)) + min)
-        i++
-    };
-//zombie speed
-setInterval(function () {
-    animate()
-    function animate() {
-        var min = 100;
-        var max = 500;
-        var randomNumbs = Math.floor(Math.random() * (max - min + 1)) + min + 'px';
-
-        $('.zombie').animate({ "left": randomNumbs }, 7000, function () {
-            $('.zombie').animate({ "right": randomNumbs }, 7000, function () { animate() })
-        })
-    }
-}, 10);
 
 
 
-localStorage.setItem("facing","right")
+
+
+
+localStorage.setItem("facing", "right")
+
+
+var zombieSpeed = 7000;
+var bulletSpeed = 1000;
+var bulletAndZombieIntervalSpeed = 1;
+var bc = 0;
+var zc = 0;
+
 
 function bulletSpawn() {
     $('#player').append('<div id="bullet" class="bullet">');
     if (localStorage.getItem('facing') == "right") {
-        $('.bullet').animate({ "left": "1000px" }, 15000, "linear", function () {
+        $('.bullet').animate({ "left": "1000px" }, bulletSpeed, "linear", function () {
             $(this).remove();
         });
     } else if ((localStorage.getItem('facing') == "left")) {
-        $('.bullet').animate({ "right": "1000px" }, 15000, "linear", function () {
+        $('.bullet').animate({ "right": "1000px" }, bulletSpeed, "linear", function () {
             $(this).remove();
         });
     };
@@ -135,175 +121,128 @@ function bulletSpawn() {
 
 
 //Version 0.1.4 main fix starts here.
+//-------------------------------------------------------------
 
-var bc = 0;
-//function test
-//javascript version
-//function bulletHit() {
 
-//    var bullet = document.getElementById("bullet")
-
-//    if (bullet) {
-//        bullet.id = "bullet-" + bc;
-//        bullet.style.backgroundColor = "green"
-//        bc++
-//        bulletCollision(bullet);
-//        return console.log(!!(bullet));
-
-//    } else {
-//        return console.log(!!(bullet));
-//    }
-
-//}
-
-//jQuery version
 function bulletHit() {
 
-    var bullet = $("#bullet");
-    bullet.append("<div class='stats' id='stats'></div>");
+    var b = $("#bullet");
+    b.append("<div class='stats' id='stats'></div>");
     var stats = $("#stats");
 
-    if (bullet) {
-        bullet.attr("id", "bullet-" + bc)
+    if (b) {
+        b.attr("id", "bullet-" + bc)
         stats.attr("id", "stats-" + bc)
-        bc++
 
-       
         var bulletTimer = setInterval(function () {
             hasBulletHit()
-        }, .5)
+        }, bulletAndZombieIntervalSpeed)
 
-        function hasBulletHit(){
-            var boff = bullet.offset();
-            var bleft = boff.left;
-            var bleftRound = Math.round(bleft * 100) / 100
-            stats.text(bleftRound)
+        function hasBulletHit() {
+            var boff = b.offset();
+            var x = boff.left;
+            var y = boff.top;
+            var xRound = Math.round(x * 100) / 100
+            stats.text(xRound)
+            var check = document.elementFromPoint((x + 10), y);
 
-            if(bleft > 500){
+
+            if (check == null) {
+
+                b.remove()
+                console.log("Bullet went out of bounds.")
                 clearInterval(bulletTimer)
-               // alert(bleft)
+
+
+
+
+            } else if (check.classList.contains("zombie")) {
+
+                console.log("Collision")
+                clearInterval(bulletTimer)
+                b.remove()
+
+                var z = check;
+                check.id = "dead"
+
+                var minHE = 30;
+                var maxHE = 45;
+                var random = Math.floor(Math.random() * (maxHE - minHE + 1)) + minHE;
+                var currentValue = z.getAttribute("value");
+                var deduction = z.getAttribute("value") - random;
+                if (currentValue >= 0) {
+                    z.setAttribute("value", deduction)
+                    z.innerText = deduction;
+                } else {
+                    z.innerText = "DEAD";
+                    currentScore++
+                    z.remove();
+                }
+
+
+            } else {
+                console.log("No Zombie")
+                setTimeout(function () {
+                    clearInterval(bulletTimer)
+                }, 900)
             }
         }
-
-
-
-        //var collisionTimer = setTimeout(function hasBulletHit() {
-
-        //    if (bc>40) 
-        //        return 
-
-
-
-        //    var boff = bullet.offset();
-        //    var bleft = boff.left;
-        //    var bleftRound = Math.round(bleft * 100) / 100
-        //    stats.text(bleftRound)
-
-        //    hasBulletHit();
-        //}, 1);
-
-
-
-
-
-        return(!!(bullet));
+        bc++
+        return (!!(b));
 
     } else {
-        return(!!(bullet));
+        return (!!(b));
     }
 
 }
 
-//[old code from V. 0.1.3]
-
-
-//bullet collision detection and zombie health
-//setInterval(function () {
-//    score()
-//    var b = $("#bullet");
-
-//    if (b) {
-//        var boff = b.offset();
-//        var bleft = boff.left;
-//        $('.zombie').each(function () {
-
-
-//            if (localStorage.getItem('facing') == null) {
-//                localStorage.setItem('facing', 'right')
-//            } else {
-
-//                var z = $(this);
-
-//                var zoff = z.offset();
-//                var zleft = zoff.left;
-//                var zleftLeft = zleft + 100;
-
-//                var p = $("#player");
-//                var poff = p.offset();
-//                var pleft = poff.left;
-
-//                var minHE = 30;
-//                var maxHE = 45;
-//                var random = Math.floor(Math.random() * (maxHE - minHE + 1)) + minHE;
-//                var currentValue = z.attr('value');
-//                var deduction = currentValue - random;
-
-//                if (currentValue <= 0) {
-//                    z.remove();
-//                };
-
-//                if (localStorage.getItem('facing') == "right") {
-//                    if (bleft >= (zleft + 50) && pleft < (zleft + 50)) {
-//                        b.remove();
-//                        currentScore++;
-
-//                        z.attr("value", (currentValue - random));
-//                        z.text(currentValue);
-//                    };
-
-//                };
-//                if (localStorage.getItem('facing') == "left") {
-//                    if (bleft <= (zleft + 50) && pleft > (zleft + 50)) {
-//                        b.remove();
-//                        currentScore++;
-
-
-//                        z.attr("value", (currentValue - random));
-//                        z.text(currentValue);
-
-
-//                    };
-//                };
-//            };
-
-//        });
-//    }
-//}, 100);
 
 
 
 
+function zombieSpawning() {
+    var min = 3;
+    var max = 2000;
+    var s = Math.floor(Math.random() * (max - min + 1)) + min;
+    var p = $("#player");
+    var poff = p.offset();
+    var pleft = poff.left;
+    var difference = s - pleft;
+    var Maximus = pleft + 400;
+    var Minimus = pleft - 300
 
+    if (s >= Maximus) {
+        $('#zom').prepend('<div id="zombie' + zc + '" value="100" class="zombie">');
+        $('#zombie').css("left", s + "px")
+    } else if (s <= Minimus) {
+        $('#zom').prepend('<div id="zombie' + zc + '" value="100" class="zombie">');
+        $('#zombie').css("left", s + "px")
+    }
+    else {
+        return false;
+    }
+    var min = 5;
+    var max = 30;
+    $('#zombie').css("bottom", Math.floor(Math.random() * (max - min + 1)) + min)
+    zc++
+    animateZombie();
 
+};
 
+function animateZombie() {
+    var min = 100;
+    var max = 500;
+    var randomNumbs = Math.floor(Math.random() * (max - min + 1)) + min + 'px';
 
-
-
-
-
-
-
-
-
-
-
-
-
+    $('.zombie').animate({ "left": randomNumbs }, 7000, function () {
+        $('.zombie').animate({ "right": randomNumbs }, 7000, function () { animateZombie() })
+    })
+}
 
 
 
 function score() {
-    $('.score').remove()
+    // $('.score').remove()
     $('body').append('<div class="score">Score: ' + currentScore + '</div>')
 };
 
@@ -321,47 +260,3 @@ $(function rain() {
 
 
 
-
-
-
-
-
-//--------------------------------------------------------------------------
-//Touch Controls VVV
-
-$(function () {
-    $("#rain").bind("tap", tapHandler);
-
-    function tapHandler(event) {
-        bulletSpawn();
-        bulletHit();
-    };
-});
-
-$(function () {
-    $("#right").bind("tap", tapHandler);
-    function tapHandler(event) {
-        rightArrowPressed()
-    }
-
-
-
-
-
-    $(function () {
-        $("#left").bind("tap", tapHandler);
-
-        function tapHandler(event) {
-            leftArrowPressed();
-        };
-    });
-});
-$(function () {
-    $(".startButton").bind("tap", tapHandler);
-
-    function tapHandler(event) {
-        $(this).css("visibility", "hidden")
-        $('.title').css("visibility", "hidden")
-        setInterval(function () { zombieSpawning() }, 1500);
-    };
-});
